7e59e453a3df52ec3663ad45a365c4e85f717e69
Pranav <prokulkarni1995@gmail.com>
2023-08-30 11:27:29 +0530

    add new feature

--- ven_test.py
+++ ven_test.py
@@ -1,7 +1,7 @@
 
 
 #Create the vendor class
-
+#this is the new case
 class vendor:
     def __init__(self, vendor_id,Vendor_name,vendor_sub_account_id,vendor_collect_on_delivery_ind):
         self.vendor_id = vendor_id
